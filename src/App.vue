2440059073d0f2c95ea5b869c5ee89822c436119
Daniel Wentsch <hello@wentsch.me>
2020-06-01 21:14:13 +0200

    Extract ColorCard component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="app">
         <div class="max-w-xl mx-auto">
-            <h1 class="font-serif font-black text-blue-800 text-6xl">
+            <h1 class="font-serif text-6xl font-black text-blue-800">
                 TailScale
             </h1>
             <p class="max-w-sm mt-0">
@@ -9,127 +9,82 @@
             </p>
         </div>
 
-        <div class="mt-8 mx-auto max-w-xl mx-auto">
-            <div class="rounded-lg border-4 border-yellow-300 p-4 -mx-4">
+        <div class="max-w-xl mx-auto mt-8">
+            <div class="p-4 -mx-4 border-4 border-yellow-300 rounded-lg">
                 <div class="grid grid-cols-2 gap-4">
                     <label class="block">
                         <span class="text-sm uppercase">Color Name</span>
-                        <input class="form-input mt-1 block w-full" placeholder="green">
+                        <input class="block w-full mt-1 form-input" placeholder="green">
                     </label>
 
                     <label class="block">
                         <span class="text-sm uppercase">Color Value</span>
-                        <input class="form-input mt-1 block w-full" type="text" placeholder="#48bb78">
+                        <input class="block w-full mt-1 form-input" type="text" placeholder="#48bb78">
                     </label>
                 </div>
             </div>
         </div>
 
         <div class="mt-16">
-            <h2 class="max-w-xl mx-auto font-serif font-black text-4xl text-blue-800">
-                Here’s your palette
+            <h2 class="max-w-xl mx-auto font-serif text-4xl font-black text-blue-800">
+                Here’s your Palette
             </h2>
             <p class="max-w-xl mx-auto mt-2">
                 Click to copy, edit to change.
             </p>
-            <div class="mt-6 max-w-3xl mx-auto">
-                <div class="grid gap-4 grid-cols-1 md:grid-cols-3">
-                    <div class="bg-green-100 shadow-lg rounded-lg p-4 md:p-6">
-                        <div>
-                            <p class="text-sm text-center">
-                                <span class="inline-block bg-white bg-opacity-50 py-1 px-2 rounded-sm">
-                                    green-100
-                                </span>
-                            </p>
-                            <input type="text" class="mt-2 text-sm form-input inline-block max-w-full" value="#c6f6d5">
-                        </div>
-                    </div>
-                    <div class="bg-green-200 shadow-lg rounded-lg p-4 md:p-6">
-                        <div>
-                            <p class="text-sm text-center">
-                                <span class="inline-block bg-white bg-opacity-50 py-1 px-2 rounded-sm">
-                                    green-200
-                                </span>
-                            </p>
-                            <input type="text" class="mt-2 text-sm form-input inline-block max-w-full" value="#c6f6d5">
-                        </div>
-                    </div>
-                    <div class="bg-green-300 shadow-lg rounded-lg p-4 md:p-6">
-                        <div>
-                            <p class="text-sm text-center">
-                                <span class="inline-block bg-white bg-opacity-50 py-1 px-2 rounded-sm">
-                                    green-300
-                                </span>
-                            </p>
-                            <input type="text" class="mt-2 text-sm form-input inline-block max-w-full" value="#c6f6d5">
-                        </div>
-                    </div>
-                    <div class="bg-green-400 shadow-lg rounded-lg p-4 md:p-6">
-                        <div>
-                            <p class="text-sm text-center">
-                                <span class="inline-block bg-white bg-opacity-50 py-1 px-2 rounded-sm">
-                                    green-400
-                                </span>
-                            </p>
-                            <input type="text" class="mt-2 text-sm form-input inline-block max-w-full" value="#c6f6d5">
-                        </div>
-                    </div>
-                    <div class="bg-green-500 shadow-lg rounded-lg p-4 md:p-6">
-                        <div>
-                            <p class="text-sm text-center">
-                                <span class="inline-block bg-white bg-opacity-50 py-1 px-2 rounded-sm">
-                                    green-500
-                                </span>
-                            </p>
-                            <input type="text" class="mt-2 text-sm form-input inline-block max-w-full" value="#c6f6d5">
-                        </div>
-                    </div>
-                    <div class="bg-green-600 shadow-lg rounded-lg p-4 md:p-6">
-                        <div>
-                            <p class="text-sm text-center">
-                                <span class="inline-block bg-white bg-opacity-50 py-1 px-2 rounded-sm">
-                                    green-600
-                                </span>
-                            </p>
-                            <input type="text" class="mt-2 text-sm form-input inline-block max-w-full" value="#c6f6d5">
-                        </div>
-                    </div>
-                    <div class="bg-green-700 shadow-lg rounded-lg p-4 md:p-6">
-                        <div>
-                            <p class="text-sm text-center">
-                                <span class="inline-block bg-white bg-opacity-50 py-1 px-2 rounded-sm">
-                                    green-700
-                                </span>
-                            </p>
-                            <input type="text" class="mt-2 text-sm form-input inline-block max-w-full" value="#c6f6d5">
-                        </div>
-                    </div>
-                    <div class="bg-green-800 shadow-lg rounded-lg p-4 md:p-6">
-                        <div>
-                            <p class="text-sm text-center">
-                                <span class="inline-block bg-white bg-opacity-50 py-1 px-2 rounded-sm">
-                                    green-800
-                                </span>
-                            </p>
-                            <input type="text" class="mt-2 text-sm form-input inline-block max-w-full" value="#c6f6d5">
-                        </div>
-                    </div>
-                    <div class="bg-green-900 shadow-lg rounded-lg p-4 md:p-6">
-                        <div>
-                            <p class="text-sm text-center">
-                                <span class="inline-block bg-white bg-opacity-50 py-1 px-2 rounded-sm">
-                                    green-900
-                                </span>
-                            </p>
-                            <input type="text" class="mt-2 text-sm form-input inline-block max-w-full" value="#c6f6d5">
-                        </div>
-                    </div>
+            <div class="max-w-3xl mx-auto mt-6">
+                <div class="grid grid-cols-1 gap-4 md:grid-cols-3">
+                    <color-card
+                        color-name="green"
+                        :color-shade="100"
+                        color-value="#f0fff4"
+                    />
+                    <color-card
+                        color-name="green"
+                        :color-shade="200"
+                        color-value="#c6f6d5"
+                    />
+                    <color-card
+                        color-name="green"
+                        :color-shade="300"
+                        color-value="#9ae6b4"
+                    />
+                    <color-card
+                        color-name="green"
+                        :color-shade="400"
+                        color-value="#68d391"
+                    />
+                    <color-card
+                        color-name="green"
+                        :color-shade="500"
+                        color-value="#48bb78"
+                    />
+                    <color-card
+                        color-name="green"
+                        :color-shade="600"
+                        color-value="#38a169"
+                    />
+                    <color-card
+                        color-name="green"
+                        :color-shade="700"
+                        color-value="#2f855a"
+                    />
+                    <color-card
+                        color-name="green"
+                        :color-shade="800"
+                        color-value="#276749"
+                    />
+                    <color-card
+                        color-name="green"
+                        :color-shade="900"
+                        color-value="#22543d"
+                    />
                 </div>
             </div>
         </div>
 
         <div class="mt-16">
-            <h2 class="max-w-xl mx-auto font-serif font-black text-4xl text-blue-800">
+            <h2 class="max-w-xl mx-auto font-serif text-4xl font-black text-blue-800">
                 Here are your Color Objects
             </h2>
             <div class="max-w-xl mx-auto mt-2">
@@ -137,16 +92,16 @@
                     Copy &amp; paste into <code class="text-gray-800">tailwind.config.js</code>.
                 </p>
                 <button
-                    class="mt-4 inline-flex items-center duration-200 ease-in transition rounded-sm px-4 py-2 hover:text-yellow-100 bg-yellow-300 text-xs uppercase tracking-wide hover:bg-blue-800 focus:shadow-outline focus:outline-none"
+                    class="inline-flex items-center px-4 py-2 mt-4 text-xs tracking-wide uppercase transition duration-200 ease-in bg-yellow-300 rounded-sm hover:text-yellow-100 hover:bg-blue-800 focus:shadow-outline focus:outline-none"
                 >
-                    <svg class="fill-current w-4 h-4 mr-2" viewBox="0 0 16 16"><title>note-code</title><g><path d="M10.4,15.8l-3.6-2.7l-5.5,1.8C1,15.1,0,15.1,0,14V2c0-0.6,0.4-1,1-1h2v2H2v9.6l4.7-1.6 c0.3-0.1,0.7,0,0.9,0.1l3.5,2.6l2.9-1.4V3h-1V1h2c0.6,0,1,0.4,1,1v11c0,0.4-0.2,0.7-0.6,0.9l-4,2C11.1,16,10.7,16,10.4,15.8z" /> <rect x="5" width="6" height="4" /></g></svg>
+                    <svg class="w-4 h-4 mr-2 fill-current" viewBox="0 0 16 16"><title>note-code</title><g><path d="M10.4,15.8l-3.6-2.7l-5.5,1.8C1,15.1,0,15.1,0,14V2c0-0.6,0.4-1,1-1h2v2H2v9.6l4.7-1.6 c0.3-0.1,0.7,0,0.9,0.1l3.5,2.6l2.9-1.4V3h-1V1h2c0.6,0,1,0.4,1,1v11c0,0.4-0.2,0.7-0.6,0.9l-4,2C11.1,16,10.7,16,10.4,15.8z" /> <rect x="5" width="6" height="4" /></g></svg>
                     <span>Copy to clipboard</span>
                 </button>
             </div>
 
 
-            <div class="mt-6 max-w-xl mx-auto">
-                <pre class="bg-blue-900 text-yellow-200 rounded-lg px-16 py-6">
+            <div class="max-w-xl mx-auto mt-6">
+                <pre class="px-16 py-6 text-yellow-200 bg-blue-900 rounded-lg">
 green: {
     100: '#f0fff4',
     200: '#c6f6d5',
@@ -164,7 +119,13 @@ green: {
 </template>
 
 <script>
+import ColorCard from './components/color-card';
+
 export default {
     name: 'App',
+
+    components: {
+        ColorCard,
+    },
 };
 </script>
---
+++ src/components/color-card.vue
@@ -0,0 +1,38 @@
+<template>
+    <div
+        class="flex justify-center p-4 rounded-lg shadow-lg md:p-6"
+        :style="`background-color: ${colorValue}`"
+    >
+        <div>
+            <p class="text-sm text-center">
+                <span class="inline-block px-2 py-1 bg-white bg-opacity-50 rounded-sm">
+                    {{ `${colorName}-${colorShade}` }}
+                </span>
+            </p>
+            <input
+                type="text"
+                class="inline-block max-w-full mt-2 text-sm form-input"
+                :value="colorValue"
+            >
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        colorName: {
+            type: String,
+            required: true,
+        },
+        colorShade: {
+            type: Number,
+            required: true,
+        },
+        colorValue: {
+            type: String,
+            required: true,
+        },
+    },
+};
+</script>
