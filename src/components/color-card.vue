598278d7138c18e5b936912dbbfb97bf901aedf5
Johannes Hertenstein <git@j6s.dev>
2020-06-06 12:37:34 +0200

    FEATURE | Add shade generation

--- src/components/color-card.vue
+++ src/components/color-card.vue
@@ -1,18 +1,18 @@
 <template>
     <div
         class="flex justify-center p-4 rounded-lg shadow-lg md:p-6"
-        :style="`background-color: ${value.value}`"
+        :style="`background-color: ${value}`"
     >
         <div>
             <p class="text-sm text-center">
                 <span class="inline-block px-2 py-1 bg-white bg-opacity-50 rounded-sm">
-                    {{ `${value.name}-${value.shade}` }}
+                    {{ `${colorName}-${shadeName}` }}
                 </span>
             </p>
             <input
                 type="text"
                 class="inline-block max-w-full mt-2 text-sm form-input"
-                v-model="value.value"
+                v-model="value"
             >
         </div>
     </div>
@@ -21,10 +21,9 @@
 <script>
 export default {
     props: {
-        value: {
-            type: Object,
-            required: true,
-        },
+        value: { type: String, required: true },
+        colorName: { type: String, required: true },
+        shadeName: { type: String, required: true },
     },
 
     watch: {
