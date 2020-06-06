47bd79a452709688ca86bbaf1bf0921bb574d17d
Johannes Hertenstein <git@j6s.dev>
2020-06-06 13:40:00 +0200

    TASK | Use current color for all interface elements

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-    <div id="app" :style="customProperties">
+    <div id="app" class="py-12 px-4 bg-current-100 text-current-900" :style="customProperties">
         <div class="max-w-xl mx-auto">
-            <h1 class="font-serif text-6xl font-black text-blue-800">
+            <h1 class="font-serif text-6xl font-black text-current-800">
                 TailScale
             </h1>
             <p class="max-w-sm mt-0">
@@ -30,7 +30,7 @@
         </div>
 
         <div class="mt-16">
-            <h2 class="max-w-xl mx-auto font-serif text-4xl font-black text-blue-800">
+            <h2 class="max-w-xl mx-auto font-serif text-4xl font-black text-current-800">
                 Here’s your Palette
             </h2>
             <p class="max-w-xl mx-auto mt-2">
@@ -50,7 +50,7 @@
         </div>
 
         <div class="mt-16">
-            <h2 class="max-w-xl mx-auto font-serif text-4xl font-black text-blue-800">
+            <h2 class="max-w-xl mx-auto font-serif text-4xl font-black text-current-800">
                 Here are your Color Objects
             </h2>
             <div class="max-w-xl mx-auto mt-2">
@@ -58,7 +58,7 @@
                     Copy &amp; paste into <code class="text-gray-800">tailwind.config.js</code>.
                 </p>
                 <button
-                    class="inline-flex items-center px-4 py-2 mt-4 text-xs tracking-wide uppercase transition duration-200 ease-in bg-yellow-300 rounded-sm hover:text-yellow-100 hover:bg-blue-800 focus:shadow-outline focus:outline-none"
+                    class="inline-flex items-center px-4 py-2 mt-4 text-xs tracking-wide uppercase transition duration-200 ease-in bg-current-200 rounded-sm hover:text-current-100 hover:bg-current-800 focus:shadow-outline focus:outline-none"
                 >
                     <svg class="w-4 h-4 mr-2 fill-current" viewBox="0 0 16 16"><title>note-code</title><g><path d="M10.4,15.8l-3.6-2.7l-5.5,1.8C1,15.1,0,15.1,0,14V2c0-0.6,0.4-1,1-1h2v2H2v9.6l4.7-1.6 c0.3-0.1,0.7,0,0.9,0.1l3.5,2.6l2.9-1.4V3h-1V1h2c0.6,0,1,0.4,1,1v11c0,0.4-0.2,0.7-0.6,0.9l-4,2C11.1,16,10.7,16,10.4,15.8z" /> <rect x="5" width="6" height="4" /></g></svg>
                     <span>Copy to clipboard</span>
@@ -67,7 +67,7 @@
 
 
             <div class="max-w-xl mx-auto mt-6">
-                <pre class="px-16 py-6 text-yellow-200 bg-blue-900 rounded-lg">
+                <pre class="px-16 py-6 text-current-100 bg-current-900 rounded-lg">
 green: {
     100: '#f0fff4',
     200: '#c6f6d5',
