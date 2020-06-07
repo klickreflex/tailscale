d1974204f63fa4c75e2d94f4d319a05457d2a03b
Johannes Hertenstein <git@j6s.dev>
2020-06-07 15:50:59 +0200

    CLEANUP | Extract functionality from main component
    The main app component was getting a bit crowded for my taste. This
    commit
    
    * Moves the actual generation logic into it's own file
    * Moves the preset list into it's own component
    
    Now the main app component is a bit leaner again.

---
+++ src/components/preset-list.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex justify-between">
+        <button
+            class="w-8 h-8 mx-2 rounded border-yellow-300"
+            :class="{
+                'border-2': baseColor === preset.baseColor,
+            }"
+            v-for="preset in presets"
+            :style="{ backgroundColor: preset.baseColor }"
+            @click="applyPreset(preset)"
+        />
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        presets: { type: Array, required: true },
+        baseColor: { type: String, required: true },
+    },
+};
+</script>
