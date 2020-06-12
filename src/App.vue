3b6cea4a893622c94922176614bf73e201fb8674
Daniel Wentsch <hello@wentsch.me>
2020-06-12 20:08:31 +0200

    Fiddle around

--- src/App.vue
+++ src/App.vue
@@ -1,98 +1,103 @@
 <template>
     <div id="app" :style="customProperties">
-        <div class="max-w-xl mx-auto">
-            <h1 class="font-serif text-6xl font-black text-blue-800">
-                TailScale
-            </h1>
-            <p class="max-w-sm mt-0">
-                Generate TailwindCSS-compatible color shades from a base color.
-            </p>
-        </div>
+        <div class="flex">
+            <!-- Configuration panel -->
+            <div>
+                <div class="max-w-xl">
+                    <h1 class="font-serif text-6xl font-black text-blue-800">
+                        TailScale
+                    </h1>
+                    <p class="max-w-sm mt-0">
+                        Generate TailwindCSS-compatible color shades from a base color.
+                    </p>
+                </div>
 
-        <preset-list
-            :presets="presets"
-            :base-color="baseColor"
-            @apply="applyPreset"
-            class="max-w-xl mx-auto mt-8 "
-        />
+                <preset-list
+                    :presets="presets"
+                    :base-color="baseColor"
+                    @apply="applyPreset"
+                    class="max-w-xl mt-8 "
+                />
 
-        <div class="max-w-xl mx-auto mt-4">
-            <div class="p-4 -mx-4 border-4 rounded-lg border-current-300">
-                <div class="grid grid-cols-2 gap-4">
-                    <div>
-                        <label class="block">
-                            <span class="text-sm uppercase">Color Name</span>
-                            <input
-                                class="block w-full mt-1 form-input"
-                                placeholder="green"
-                                v-model="colorName"
-                            >
-                        </label>
+                <div class="max-w-xl mt-4">
+                    <div class="p-4 -mx-4 border-4 border-yellow-300 rounded-lg">
+                        <div class="grid grid-cols-2 gap-4">
+                            <div>
+                                <label class="block">
+                                    <span class="text-sm uppercase">Color Name</span>
+                                    <input
+                                        class="block w-full mt-1 form-input"
+                                        placeholder="green"
+                                        v-model="colorName"
+                                    >
+                                </label>
 
-                        <color-name-suggestion
-                            v-model="colorName"
-                            :base-color="baseColor"
-                            :presets="presets"
-                            class="mt-4"
-                        />
+                                <color-name-suggestion
+                                    v-model="colorName"
+                                    :base-color="baseColor"
+                                    :presets="presets"
+                                    class="mt-4"
+                                />
+                            </div>
+
+
+                            <label class="block">
+                                <span class="text-sm uppercase">Color Value</span>
+                                <color-picker v-model="baseColor" />
+                            </label>
+                        </div>
+
+                        <div class="mt-4">
+                            <span class="text-sm uppercase">Brightness Spread</span>
+                            <slider
+                                v-model="spread"
+                                :min="-100"
+                                :max="100"
+                                :min-range="100"
+                                :tooltip-formatter="value => `${value}%`"
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mt-16">
+                    <h2 class="max-w-xl font-serif text-4xl font-black text-blue-800">
+                        Here’s your Palette
+                    </h2>
+                    <p class="max-w-xl mt-2">
+                        Click to copy, edit to change.
+                    </p>
+                    <div class="max-w-xl mx-auto mt-6">
+                        <div class="grid grid-cols-1 gap-4 md:grid-cols-3">
+                            <color-card
+                                v-for="(color, shade) in shades"
+                                v-model="shades[shade]"
+                                :color-name="colorName"
+                                :shade-name="shade"
+                                :key="shade"
+                            />
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mt-16">
+                    <h2 class="max-w-xl mx-auto font-serif text-4xl font-black text-blue-800">
+                        Here are your Color Objects
+                    </h2>
+                    <div class="max-w-xl mx-auto mt-2">
+                        <p>
+                            Copy &amp; paste into <code class="text-gray-800">tailwind.config.js</code>.
+                        </p>
+                        <copy-to-clipboard-button :content-to-copy="tailwindConfig">
+                            Copy to clipboard
+                        </copy-to-clipboard-button>
                     </div>
 
 
-                    <label class="block">
-                        <span class="text-sm uppercase">Color Value</span>
-                        <color-picker v-model="baseColor" />
-                    </label>
+                    <div class="max-w-xl mx-auto mt-6">
+                        <pre class="px-16 py-6 text-yellow-200 bg-blue-900 rounded-lg">{{ tailwindConfig }}</pre>
+                    </div>
                 </div>
-
-                <div class="mt-4">
-                    <span class="text-sm uppercase">Brightness Spread</span>
-                    <slider
-                        v-model="spread"
-                        :min="-100"
-                        :max="100"
-                        :min-range="100"
-                        :tooltip-formatter="value => `${value}%`"
-                    />
-                </div>
-            </div>
-        </div>
-
-        <div class="mt-16">
-            <h2 class="max-w-xl mx-auto font-serif text-4xl font-black text-blue-800">
-                Here’s your Palette
-            </h2>
-            <p class="max-w-xl mx-auto mt-2">
-                Click to copy, edit to change.
-            </p>
-            <div class="max-w-3xl mx-auto mt-6">
-                <div class="grid grid-cols-1 gap-4 md:grid-cols-3">
-                    <color-card
-                        v-for="(color, shade) in shades"
-                        v-model="shades[shade]"
-                        :color-name="colorName"
-                        :shade-name="shade"
-                        :key="shade"
-                    />
-                </div>
-            </div>
-        </div>
-
-        <div class="mt-16">
-            <h2 class="max-w-xl mx-auto font-serif text-4xl font-black text-blue-800">
-                Here are your Color Objects
-            </h2>
-            <div class="max-w-xl mx-auto mt-2">
-                <p>
-                    Copy &amp; paste into <code class="text-gray-800">tailwind.config.js</code>.
-                </p>
-                <copy-to-clipboard-button :content-to-copy="tailwindConfig">
-                    Copy to clipboard
-                </copy-to-clipboard-button>
-            </div>
-
-
-            <div class="max-w-xl mx-auto mt-6">
-                <pre class="px-16 py-6 text-yellow-200 bg-blue-900 rounded-lg">{{ tailwindConfig }}</pre>
             </div>
         </div>
     </div>
